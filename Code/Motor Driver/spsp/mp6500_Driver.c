2d0f063e023e80819f5dd2c725114b584068e8e3
Dstebz <daniel.stebbings.2021@uni.strath.ac.uk>
2024-02-26 18:25:51 +0000

    Initial work on motor driver

---
+++ Code/Motor Driver/spsp/mp6500_Driver.c
@@ -0,0 +1,13 @@
+/*
+ * mp6500_Driver.c
+ *
+ *  Created on: 24 Feb 2024
+ *      Author: Daniel
+ */
+
+#include "mp6500_driver.h"
+
+
+void drive(int angle)
+
+
---
+++ Code/Motor Driver/spsp/mp6500_driver.h
@@ -0,0 +1,15 @@
+/*
+ * mp6500_driver.h
+ *
+ *  Created on: 24 Feb 2024
+ *      Author: Daniel
+ */
+
+#ifndef MP6500_DRIVER_H_
+#define MP6500_DRIVER_H_
+
+void drive(int angle)
+
+
+
+#endif /* MP6500_DRIVER_H_ */
